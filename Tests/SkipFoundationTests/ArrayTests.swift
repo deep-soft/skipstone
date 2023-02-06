97b5eb46a376b6a1ca8ce36f77a2487fb860fe6c
Abe White <abewhite@mac.com>
2023-02-06 17:37:59 -0600

    Run array tests in Kotlin

---
+++ Tests/SkipFoundationTests/ArrayTests.swift
@@ -0,0 +1,31 @@
+#if !SKIP
+@testable import SkipFoundation
+import SkipTest
+#endif
+
+final class ArrayTests: XCTestCase {
+    func testArrayLiteralInit() {
+        let emptyArray: [Int] = []
+        XCTAssertEqual(emptyArray.count, 0)
+
+        let emptyArray2: Array<Int> = []
+        XCTAssertEqual(emptyArray2.count, 0)
+
+        let singleElementArray = [1]
+        XCTAssertEqual(singleElementArray.count, 1)
+
+        let multipleElementArray = [1, 2]
+        XCTAssertEqual(multipleElementArray.count, 2)
+    }
+
+    func testArrayAppend() {
+        var array = [1, 2]
+        array.append(3)
+        XCTAssertEqual(array.count, 3)
+
+        var array2 = array
+        array2.append(4)
+        XCTAssertEqual(array2.count, 4)
+//        XCTAssertEqual(array.count, 3)
+    }
+}
