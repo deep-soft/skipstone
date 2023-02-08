79d4742dfc1397bed67e61ea6bbad946cb38c64f
marcprux <mwp1@cornell.edu>
2023-02-08 18:07:21 -0500

    Add SkipData and tests

---
+++ Sources/SkipFoundation/Data.swift
@@ -0,0 +1,76 @@
+#if !SKIP
+import struct Foundation.Data
+public typealias Data = Foundation.Data
+public typealias PlatformData = Foundation.Data
+#else
+public typealias Data = SkipData
+public typealias PlatformData = kotlin.ByteArray
+#endif
+
+
+// SKIP REPLACE: @JvmInline public value class SkipData(val rawValue: PlatformData) { companion object { } }
+public struct SkipData : RawRepresentable {
+    public let rawValue: PlatformData
+
+    public init(rawValue: PlatformData) {
+        self.rawValue = rawValue
+    }
+
+    public init(_ rawValue: PlatformData) {
+        self.rawValue = rawValue
+    }
+}
+
+#if !SKIP
+
+#warning("FIXME: restore throws once we can remove `try`")
+
+extension Data {
+    /// static init until constructor overload works
+    public static func contentsOfFile(filePath: String) -> Data {
+        return try! NSData(contentsOfFile: filePath) as Data
+    }
+
+    public static func contentsOfURL(url: URL) -> Data {
+        return try! NSData(contentsOf: url) as Data
+    }
+}
+
+#else
+
+/// A byte buffer in memory.
+///
+/// This is a `Foundation.Data` wrapper around `kotlin.ByteArray`.
+extension SkipData {
+    public init(rawValue: PlatformData) {
+        self.rawValue = rawValue
+    }
+
+    /// static init until constructor overload works
+    public static func contentsOfFile(filePath: String) throws -> Data {
+        return Data(java.io.File(filePath).readBytes())
+    }
+
+    /// static init until constructor overload works
+    public static func contentsOfURL(url: URL) throws -> Data {
+//        if url.isFileURL {
+//            return Data(java.io.File(url.path).readBytes())
+//        } else {
+//        return Data(url.rawValue.openConnection().getInputStream().readBytes())
+//        }
+
+        // this seems to work for both file URLs and network URLs
+        return Data(url.rawValue.readBytes())
+    }
+
+    /// Foundation uses `count`, Java uses `size`.
+    public var count: Int { return rawValue.size }
+}
+
+public extension String {
+    public static func `init`(contentsOfURL url: URL) throws -> String {
+        return url.rawValue.readText()
+    }
+}
+
+#endif
---
+++ Tests/SkipFoundationTests/DataTests.swift
@@ -0,0 +1,31 @@
+#if !SKIP
+@testable import SkipFoundation
+import XCTest
+#endif
+
+final class DataTests: XCTestCase {
+    var logger = Logger(subsystem: "test", category: "DataTests")
+
+    func testData() throws {
+        let hostsFile: URL = URL.init(fileURLWithPath: "/etc/hosts", isDirectory: false)
+
+        let hostsData: Data = Data.contentsOfURL(url: hostsFile)
+        XCTAssertNotEqual(0, hostsData.count)
+
+        // FIXME: force-unwrap doesn't transpile
+        // SKIP REPLACE: val url: URL = URL.init(string = "https://www.example.com")
+        let url: URL = URL.init(string: "https://www.example.com")!
+        let urlData: Data = Data.contentsOfURL(url: url)
+
+        logger.log("downloaded url size: \(urlData.count)") // ~1256
+        XCTAssertNotEqual(0, urlData.count)
+
+        // FIXME: force-unwrap doesn't transpile
+        // SKIP REPLACE: val url2: URL = URL.init(string = "domains/reserved", relativeTo = URL.init(string = "https://www.iana.org"))
+        let url2: URL = URL.init(string: "domains/reserved", relativeTo: URL.init(string: "https://www.iana.org"))!
+        let url2Data: Data = Data.contentsOfURL(url: url2)
+
+        logger.log("downloaded url2 size: \(url2Data.count)") // ~1256
+        XCTAssertNotEqual(0, url2Data.count)
+    }
+}
