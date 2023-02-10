ee1c19422052ace0bc2c68f7b8d2950f81c64a42
Abe White <abewhite@mac.com>
2023-02-10 10:49:09 -0600

    Snapshot symbol work

--- Sources/Skip/SymbolInfo.swift
+++ Sources/Skip/SymbolInfo.swift
@@ -10,34 +10,40 @@ public class SymbolInfo {
     /// The current module name.
     public let moduleName: String
 
-    private var moduleInfos: [String: ModuleInfo] = [:]
+    private var symbolsByIdentifier: [String: Symbol] = [:]
+    private var symbolsByName: [String: [Symbol]] = [:]
 
     public init(moduleName: String, graphs: [String: UnifiedSymbolGraph]) {
         self.moduleName = moduleName
         for entry in graphs {
-            let moduleInfo = Self.processModuleGraph(entry.value, publicOnly: entry.key != moduleName)
-            moduleInfos[entry.key] = moduleInfo
+            dump(entry.value) //~~~
+            processGraph(entry.value, module: entry.key)
         }
     }
 
-    private static func processModuleGraph(_ graph: UnifiedSymbolGraph, publicOnly: Bool) -> ModuleInfo {
-        let moduleInfo = ModuleInfo()
+    private func processGraph(_ graph: UnifiedSymbolGraph, module: String) {
         for entry in graph.symbols {
-            guard let symbolInfo = processSymbol(entry.value, publicOnly: publicOnly) else {
+            guard let symbol = processSymbol(entry.value, module: module) else {
                 continue
             }
-            moduleInfo.addSymbol(symbolInfo, identifier: entry.key)
+            symbolsByIdentifier[symbol.identifier] = symbol
+
+            var symbolsWithName = symbolsByName[symbol.name] ?? []
+            symbolsWithName.append(symbol)
+            symbolsByName[symbol.name] = symbolsWithName
         }
-        return moduleInfo
     }
 
-    private static func processSymbol(_ symbol: UnifiedSymbolGraph.Symbol, publicOnly: Bool) -> SymbolInfo? {
+    private func processSymbol(_ symbol: UnifiedSymbolGraph.Symbol, module: String) -> Symbol? {
         guard let selector = symbol.accessLevel.keys.first(where: { $0.interfaceLanguage == "swift" }) else {
             return nil
         }
-        //~~~
-        // TODO: Will this skip default public members? e.g. members of a public protocol
+        let publicOnly = module == moduleName
         guard !publicOnly || symbol.accessLevel[selector]?.rawValue == "public" || symbol.accessLevel[selector]?.rawValue == "open" else {
+            return nil
+        }
+
+        guard let mixins = symbol.mixins[selector], let declarationFragments = mixins[SymbolGraph.Symbol.DeclarationFragments.mixinKey] as? SymbolGraph.Symbol.DeclarationFragments else {
             return nil
         }
         guard let kind = symbol.kind[selector] else {
@@ -52,6 +58,9 @@ public class SymbolInfo {
             print("ENUM")
         case .func:
             print("FUNC")
+            guard let functionSignature = mixins[SymbolGraph.Symbol.FunctionSignature.mixinKey] as? SymbolGraph.Symbol.FunctionSignature else {
+                return nil
+            }
         case .`init`:
             print("INIT")
         case .extension:
@@ -61,7 +70,7 @@ public class SymbolInfo {
         case .operator:
             print("OPERATOR")
         case .property:
-            print("PROPERTY")
+            return Property(symbol: symbol, selector: selector, declarationFragments: declarationFragments)
         case .protocol:
             print("PROTOCOL")
         case .struct:
@@ -71,19 +80,35 @@ public class SymbolInfo {
         case .var:
             print("VAR")
         default:
-            print("UNKNOWN SYMBOL KIND: \(kind.identifier)")
+            return nil
         }
         print("SYMBOL: \(symbol)")
         return nil
     }
+}
 
-    class ModuleInfo {
-        func addSymbol(_ symbol: SymbolInfo, identifier: String) {
+private class Module {
+    func addSymbol(_ symbol: Symbol, identifier: String) {
 
-        }
+    }
+}
+
+private protocol Symbol {
+
+}
+
+private struct Property: Symbol {
+    init?(symbol: UnifiedSymbolGraph.Symbol, selector: UnifiedSymbolGraph.Selector, declarationFragments: SymbolGraph.Symbol.DeclarationFragments) {
+
+    }
+}
+
+private extension SymbolGraph.Symbol.DeclarationFragments {
+    var identifier: String? {
+        return declarationFragments.first(where: { $0.kind == .identifier })?.spelling
     }
 
-    struct SymbolInfo {
-
+    func hasKeyword(_ keyword: String) -> Bool {
+        return declarationFragments.contains { $0.kind == .keyword && $0.spelling == keyword }
     }
 }
