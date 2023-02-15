0b3a3e9f71c3cba96e014a63f4512db40440c4ff
Abe White <abewhite@mac.com>
2023-02-15 13:04:14 -0600

    Add optional symbols to all SkipTests tests

--- Tests/SkipTests/SymbolsTests.swift
+++ Tests/SkipTests/SymbolsTests.swift
@@ -4,23 +4,8 @@ import SymbolKit
 import XCTest
 
 final class SymbolsTests: XCTestCase {
-    let symbolCache = SymbolCache()
-    var symbols: Symbols!
-
-    override func setUp() async throws {
-        let collector = GraphCollector(extensionGraphAssociationStrategy: .extendingGraph)
-        for entry in try await symbolCache.symbols(for: "SkipKotlin", accessLevel: "public") {
-            collector.mergeSymbolGraph(entry.value, at: entry.key)
-        }
-        for entry in try await symbolCache.symbols(for: "SkipTests", accessLevel: "private") {
-            collector.mergeSymbolGraph(entry.value, at: entry.key)
-        }
-        let (unifiedGraphs, _) = collector.finishLoading()
-        symbols = Symbols(moduleName: "SkipTests", graphs: unifiedGraphs)
-    }
-
-    func testHasMutableValueType() {
-        let context = symbols.context()
+    func testHasMutableValueType() async throws {
+        let context = try await symbols.context()
         XCTAssertNil(context.isMutableValueType(qualifiedName: "NonExistantTypeName"))
 
         XCTAssertEqual(false, context.isMutableValueType(qualifiedName: "SymbolsTestsClass"))
@@ -36,16 +21,16 @@ final class SymbolsTests: XCTestCase {
         XCTAssertEqual(false, context.isMutableValueType(qualifiedName: "SymbolsTestsTransitiveAnyObjectRestrictedProtocol"))
     }
 
-    func testIdentifierType() {
-        let context = symbols.context()
+    func testIdentifierType() async throws {
+        let context = try await symbols.context()
         XCTAssertEqual(.string, context.type(of: "symbolsTestsVar"))
         XCTAssertEqual(.array(.int), context.type(of: "symbolsTestsArrayVar"))
         XCTAssertEqual(.dictionary(.string, .int), context.type(of: "symbolsTestsDictionaryVar"))
         XCTAssertEqual(.named("SymbolsTestsClass", []), context.type(of: "symbolsTestsNamedVar"))
     }
 
-    func testMemberType() {
-        let context = symbols.context()
+    func testMemberType() async throws {
+        let context = try await symbols.context()
         XCTAssertEqual(.int, context.type(of: "count", in: .array(.int)))
 
         XCTAssertEqual(.int, context.type(of: "letVar", in: .named("SymbolsTestsImmutableStruct", [])))
@@ -59,22 +44,22 @@ final class SymbolsTests: XCTestCase {
         XCTAssertEqual(.string, context.type(of: "s", in: .tuple(["i", "s"], [.int, .string])))
     }
 
-    func testSubscript() {
-        let context = symbols.context()
+    func testSubscript() async throws {
+        let context = try await symbols.context()
         XCTAssertEqual([.function([.int], .int)], context.subscriptSignature(in: .array(.int), arguments: [LabeledValue<TypeSignature>(label: nil, value: .int)]))
         XCTAssertEqual([.function([.string], .int)], context.subscriptSignature(in: .dictionary(.string, .int), arguments: [LabeledValue<TypeSignature>(label: nil, value: .int)]))
     }
 
-    func testFunction() {
-        let context = symbols.context()
+    func testFunction() async throws {
+        let context = try await symbols.context()
         XCTAssertEqual([.function([], .void)], context.functionSignature(of: "voidF", in: .named("SymbolsTestsClass", []), arguments: []))
 
         XCTAssertEqual([.function([.int, .string], .int)], context.functionSignature(of: "baseF", in: .named("SymbolsTestsClass", []), arguments: [LabeledValue<TypeSignature>(label: nil, value: .none), LabeledValue<TypeSignature>(label: "p2", value: .none)]))
         XCTAssertEqual([.function([.int], .int)], context.functionSignature(of: "baseF", in: .named("SymbolsTestsClass", []), arguments: [LabeledValue<TypeSignature>(label: nil, value: .none)]))
     }
 
-    func testTrailingClosures() {
-        let context = symbols.context()
+    func testTrailingClosures() async throws {
+        let context = try await symbols.context()
         XCTAssertEqual([.function([.int, .function([.string], .int)], .string)], context.functionSignature(of: "trailingClosureF1", in: .named("SymbolsTestsClass", []), arguments: [LabeledValue<TypeSignature>(label: "p1", value: .none), LabeledValue<TypeSignature>(label: "tc1", value: .none)]))
 
         let f2Type: TypeSignature = .function([.string, .function([.string, .string], .int), .function([], .void)], .void)
--- Tests/SkipTests/XCTestCaseAdditions.swift
+++ Tests/SkipTests/XCTestCaseAdditions.swift
@@ -1,22 +1,48 @@
 @testable import Skip
+import SkipBuild
+import SymbolKit
 import XCTest
 
 extension XCTestCase {
-    /// Checks that the given Swift compiles to the specified Kotlin.
-    public func check(expectFailure: Bool = false, swift: String, kotlin: String? = nil, file: StaticString = #file, line: UInt = #line) async throws {
-        let srcFile = try tmpFile(named: "Source.swift", contents: swift)
-        if let kotlin = kotlin {
-            let tp = Transpiler(sourceFiles: [Source.File(path: srcFile.path)])
-            try await tp.transpile { transpilation in
-                //print("transpilation:", transpilation.output)
-                var content = transpilation.output.content
-                let autoImportPrefix = "import skip.kotlin."
-                content = content.split(separator: "\n", omittingEmptySubsequences: false).filter({ !$0.hasPrefix(autoImportPrefix) }).joined(separator: "\n")
-                if expectFailure {
-                    XCTExpectFailure()
-                }
-                XCTAssertEqual(kotlin.trimmingCharacters(in: .whitespacesAndNewlines), content.trimmingCharacters(in: .whitespacesAndNewlines), file: file, line: line)
+    var symbols: Symbols {
+        get async throws {
+            if let symbols = Self.symbols {
+                return symbols
             }
+
+            let symbolCache = SymbolCache()
+            let collector = GraphCollector(extensionGraphAssociationStrategy: .extendingGraph)
+            for entry in try await symbolCache.symbols(for: "SkipKotlin", accessLevel: "public") {
+                collector.mergeSymbolGraph(entry.value, at: entry.key)
+            }
+            for entry in try await symbolCache.symbols(for: "SkipTests", accessLevel: "private") {
+                collector.mergeSymbolGraph(entry.value, at: entry.key)
+            }
+            let (unifiedGraphs, _) = collector.finishLoading()
+            let symbols = Symbols(moduleName: "SkipTests", graphs: unifiedGraphs)
+            Self.symbols = symbols
+            return symbols
+        }
+    }
+    private static var symbols: Symbols?
+
+    /// Checks that the given Swift compiles to the specified Kotlin.
+    public func check(expectFailure: Bool = false, symbols: Symbols? = nil, swift: String, kotlin: String? = nil, file: StaticString = #file, line: UInt = #line) async throws {
+        guard let kotlin else {
+            return
+        }
+
+        let srcFile = try tmpFile(named: "Source.swift", contents: swift)
+        let tp = Transpiler(sourceFiles: [Source.File(path: srcFile.path)], symbols: symbols)
+        try await tp.transpile { transpilation in
+            //print("transpilation:", transpilation.output)
+            var content = transpilation.output.content
+            let autoImportPrefix = "import skip.kotlin."
+            content = content.split(separator: "\n", omittingEmptySubsequences: false).filter({ !$0.hasPrefix(autoImportPrefix) }).joined(separator: "\n")
+            if expectFailure {
+                XCTExpectFailure()
+            }
+            XCTAssertEqual(kotlin.trimmingCharacters(in: .whitespacesAndNewlines), content.trimmingCharacters(in: .whitespacesAndNewlines), file: file, line: line)
         }
     }
 
