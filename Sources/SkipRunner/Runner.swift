6d31604198a548cc47c7a3858c0543a4fc1b5ceb
marcprux <mwp1@cornell.edu>
2023-03-03 20:58:37 -0500

    Release 0.0.38

--- Sources/SkipRunner/Runner.swift
+++ Sources/SkipRunner/Runner.swift
@@ -6,7 +6,7 @@ import ArgumentParser
 import SkipBuild
 
 /// The current versio of the tool
-public let skipVersion = "0.0.37"
+public let skipVersion = "0.0.38"
 
 struct MyCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
