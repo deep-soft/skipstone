a8c0489689a5128c5c8df79808b14b3bbb4f640a
marcprux <mwp1@cornell.edu>
2023-04-12 11:52:58 -0400

    Update release script

--- scripts/release_skip.sh
+++ scripts/release_skip.sh
@@ -112,7 +112,8 @@ sed -I '' 's;package.targets += \[.binaryTarget.*;package.targets += [.binaryTar
 # this aligns the 
 cd ${SKIPPKGDIR}
 
-sed -I '' 's;.package(url: "https://github.com/skiptools/skip", from: ".*");.package(url: "https://github.com/skiptools/skip", from: "'${SEMVER_NEXT}'");g' README.md ../skiphub/Package.swift
+sed -I '' 's;.package(url: "https://github.com/skiptools/skip", from: ".*");.package(url: "https://github.com/skiptools/skip", from: "'${SEMVER_NEXT}'");g' "README.md"
+sed -I '' 's;.package(url: "https://github.com/skiptools/skip", from: ".*");.package(url: "https://github.com/skiptools/skip", from: "'${SEMVER_NEXT}'");g' ../skiphub/Package.swift
 
 # also grab the latest skiphub version and update it in the README
 SKIPHUB_VERSION=`git ls-remote --tags https://github.com/skiptools/skiphub | awk -F/ '$NF ~ /^v?[0-9]+\.[0-9]+\.[0-9]+$/ {print $NF}' | sort -V | tail -n1`
