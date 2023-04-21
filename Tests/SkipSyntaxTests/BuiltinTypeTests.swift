16b639b0e14e516d59d329f1135188e4c79d9a25
Abe White <abewhite@mac.com>
2023-04-21 11:29:44 -0500

    Minor testing reorg

--- Tests/SkipSyntaxTests/BuiltinTypeTests.swift
+++ Tests/SkipSyntaxTests/BuiltinTypeTests.swift
@@ -156,6 +156,124 @@ final class BuiltinTypeTests: XCTestCase {
         """)
     }
 
+    func testIntLiteral() async throws {
+        try await check(swift: """
+        123
+        """, kotlin: """
+        123
+        """)
+
+        try await check(swift: """
+        -123
+        """, kotlin: """
+        -123
+        """)
+
+        try await check(swift: """
+        123_000_000
+        """, kotlin: """
+        123_000_000
+        """)
+    }
+
+    func testStringLiteral() async throws {
+        try await check(swift: """
+        "abc"
+        """, kotlin: """
+        "abc"
+        """)
+
+        try await check(swift: """
+        "1 + 1 = \\(1 + 1)"
+        """, kotlin: """
+        "1 + 1 = ${1 + 1}"
+        """)
+
+        try await check(swift: """
+        "i = \\(i)"
+        """, kotlin: """
+        "i = ${i}"
+        """)
+
+        try await check(swift: """
+        "It costs ${x}"
+        """, kotlin: """
+        "It costs \\${x}"
+        """)
+    }
+
+    func testRawStringLiteral() async throws {
+        try await check(swift: """
+        #"{"name":"John Smith","isEmployed":true,"age":30}"#
+        """, kotlin: """
+        \"""{"name":"John Smith","isEmployed":true,"age":30}""\"
+        """)
+    }
+
+    func testArrayLiteral() async throws {
+        try await check(swift: """
+        {
+            let a = [1, 2, 3]
+        }
+        """, kotlin: """
+        {
+            val a = arrayOf(1, 2, 3)
+        }
+        """)
+
+        try await check(swift: """
+        {
+            let a: [Int] = [x, y, z]
+        }
+        """, kotlin: """
+        {
+            val a: Array<Int> = arrayOf(x, y, z)
+        }
+        """)
+
+        try await check(swift: """
+        {
+            let a = [Int]()
+        }
+        """, kotlin: """
+        {
+            val a = Array<Int>()
+        }
+        """)
+    }
+
+    func testDictionaryLiteral() async throws {
+        try await check(swift: """
+        {
+            let d = [1: "a", 2: "b", 3: "c"]
+        }
+        """, kotlin: """
+        {
+            val d = dictionaryOf(Tuple2(1, "a"), Tuple2(2, "b"), Tuple2(3, "c"))
+        }
+        """)
+
+        try await check(swift: """
+        {
+            let d: [Int: String] = [x: a, y: b, z: c]
+        }
+        """, kotlin: """
+        {
+            val d: Dictionary<Int, String> = dictionaryOf(Tuple2(x, a), Tuple2(y, b), Tuple2(z, c))
+        }
+        """)
+
+        try await check(swift: """
+        {
+            let d = [Int: String]()
+        }
+        """, kotlin: """
+        {
+            val d = Dictionary<Int, String>()
+        }
+        """)
+    }
+
     func testArrayLiteralToSetMapping() async throws {
         try await check(supportingSwift: """
         func setf(set: Set<Int>) {
