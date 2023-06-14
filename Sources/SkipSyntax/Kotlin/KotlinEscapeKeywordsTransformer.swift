6fa7c40b7176ccf1ab6b4b4b850dc9cb5714e91e
Abe White <abewhite@mac.com>
2023-06-14 12:08:55 -0500

    Perform keyword escaping for associated values of enum cases

--- Sources/SkipSyntax/Kotlin/KotlinEscapeKeywordsTransformer.swift
+++ Sources/SkipSyntax/Kotlin/KotlinEscapeKeywordsTransformer.swift
@@ -42,6 +42,7 @@ private class EscapeKeywordsVisitor {
     func visit(_ node: KotlinSyntaxNode) -> VisitResult<KotlinSyntaxNode> {
         if let node = node as? KotlinEnumCaseDeclaration {
             node.caseName = fixKeyword(name: node.name)
+            node.associatedValues = node.associatedValues.map(fixParameter)
         } else if let node = node as? KotlinIdentifier {
             node.name = fixKeyword(name: node.name)
         } else if let node = node as? KotlinMemberAccess {
