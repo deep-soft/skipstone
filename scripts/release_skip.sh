925277f16a3b2723e0dff359a77f3ee2097d37d8
marcprux <mwp1@cornell.edu>
2023-08-15 23:49:53 -0400

    Skip build script update Cask

--- scripts/release_skip.sh
+++ scripts/release_skip.sh
@@ -23,7 +23,8 @@
 
 # cannot run unless there are currently no diffs; override by running: 
 # SKIPDIFF=true ./scripts/release_skip.sh
-eval ${SKIPDIFF:-"git diff --exit-code"}
+#eval ${SKIPDIFF:-"git diff --exit-code"}
+set -o pipefail
 
 SKIPCONFIG=${SKIPCONFIG:-"release"}
 
@@ -34,36 +35,36 @@ ARTIFACTTOOL=skipstone
 ARTIFACT=${ARTIFACTTOOL}
 ARTIFACTBUNDLE="${ARTIFACT}.artifactbundle"
 PLUGIN_BASENAME="${ARTIFACT}.plugin"
+PLUGIN_ZIP="${PLUGIN_BASENAME}.zip"
 
 DIR=.build/artifactbundle
 GITDATE="$(git log -1 --format=%ad --date=iso-strict)"
 GITREF="$(git rev-parse HEAD)"
+RELSTAGING=`mktemp -d`
 
 # the relative path to the repo that hosts the plug-in code
 # and references the binary executable
 SKIPPKG="../skip/Package.swift"
+SKIPSTONEDIR="../skipstone"
 SKIPPKGDIR=$(dirname ${SKIPPKG})
 SKIPHUBPKG="../skiphub/Package.swift"
+SKIPBREWDIR="../homebrew-skip"
 
 # once we get this repo sync'd, we can rely on both tags being the same
 cd ${SKIPPKGDIR}
-SEMVER_CURRENT=$(git tag -l --sort=-version:refname | grep '[0-9]*\.[0-9]*\.[0-9]*' | head -n 1)
-SEMVER_NEXT=$(semver bump "${SEMVER_BUMP:-patch}" "${SEMVER_CURRENT}")
+SKIP_VERSION_OLD=$(git tag -l --sort=-version:refname | grep '[0-9]*\.[0-9]*\.[0-9]*' | head -n 1)
+SKIP_VERSION=$(semver bump "${SEMVER_BUMP:-patch}" "${SKIP_VERSION_OLD}")
 
 cd '-'
 
-echo "Creating release and tagging new skip version: ${SEMVER_NEXT}"
+echo "Creating release and tagging new skip version: ${SKIP_VERSION}"
 
 # mark the internal version
-VERSION_PATH="Sources/SkipSyntax/Version.swift"
-
-sed -I '' 's;public let skipVersion = .*;public let skipVersion = "'${SEMVER_NEXT}'";g' "${VERSION_PATH}"
-
-git diff "${VERSION_PATH}"
+SKIPSTONE_VERSION_PATH="Sources/SkipSyntax/Version.swift"
+sed -I '' 's;public let skipVersion = .*;public let skipVersion = "'${SKIP_VERSION}'";g' "${SKIPSTONE_VERSION_PATH}"
+git diff "${SKIPSTONE_VERSION_PATH}"
 
 swift build --arch arm64 --arch x86_64 --configuration ${SKIPCONFIG} --product ${PRODUCT}
-
-set -o pipefail
 
 # try to back up any old artifactbundle folder
 mv -f ${DIR}/${ARTIFACTBUNDLE} ${DIR}/${ARTIFACTBUNDLE}.bk.`date +%s` || true
@@ -80,7 +81,7 @@ cat > ${ARTIFACTBUNDLE}/info.json << EOF
     "artifacts": {
         "${ARTIFACTTOOL}": {
             "type": "executable",
-            "version": "${SEMVER_NEXT}",
+            "version": "${SKIP_VERSION}",
             "variants": [
                 {
                     "path": "${ARTIFACTTOOL}",
@@ -97,103 +98,100 @@ du -skh "${ARTIFACTBUNDLE}"
 
 # sync file times to git date for build reproducability
 find ${ARTIFACTBUNDLE} -exec touch -d "${GITDATE:0:19}" {} \;
-zip -9 -q --symlinks -r ${PLUGIN_BASENAME}.zip ${ARTIFACTBUNDLE}
+zip -9 -q --symlinks -r ${PLUGIN_ZIP} ${ARTIFACTBUNDLE}
 
-CHECKSUM=$(shasum -a 256 ${PLUGIN_BASENAME}.zip | cut -f 1 -d ' ')
-du -skh "${PLUGIN_BASENAME}.zip"
+PLUGIN_CHECKSUM=$(shasum -a 256 ${PLUGIN_ZIP} | cut -f 1 -d ' ')
+du -skh "${PLUGIN_ZIP}"
 
 # the location of the download once we have uploaded it
-#ARTIFACT_URL="https://github.com/skiptools/skip/releases/download/${SEMVER_NEXT}/${ARTIFACTBUNDLE}.zip"
+#ARTIFACT_URL="https://github.com/skiptools/skip/releases/download/${SKIP_VERSION}/${ARTIFACTBUNDLE}.zip"
+ARTIFACT_URL="https://skip.tools/skiptools/skip/releases/download/${SKIP_VERSION}/${PLUGIN_ZIP}"
 
-ARTIFACT_URL="https://skip.tools/skiptools/skip/releases/download/${SEMVER_NEXT}/${PLUGIN_BASENAME}.zip"
+#ARTIFACT_URL="https://source.skip.tools/skip/releases/download/${SKIP_VERSION}/${PLUGIN_ZIP}"
 
-#ARTIFACT_URL="https://source.skip.tools/skip/releases/download/${SEMVER_NEXT}/${PLUGIN_BASENAME}.zip"
+cd -
 
-#gh release -R github.com/skiptools/skip delete "main-${RELNAME}" --yes || true
-
-cd '-'
-
-# package.targets += [.binaryTarget(name: "${PRODUCT}", url: "${ARTIFACT_URL}", checksum: "${CHECKSUM}")]
-sed -I '' 's;package.targets += \[.binaryTarget.*;package.targets += [.binaryTarget(name: "'${ARTIFACT}'", url: "'${ARTIFACT_URL}'", checksum: "'${CHECKSUM}'")];g' ${SKIPPKG}
-
-# First make the release in the skip plugin-in
-# this aligns the 
+# make a release of the skip command
 cd ${SKIPPKGDIR}
 
-sed -I '' 's;.package(url: "https://.*/skiptools/skip.git", from: ".*");.package(url: "https://skip.tools/skiptools/skip.git", from: "'${SEMVER_NEXT}'");g' "README.md"
-sed -I '' 's;.package(url: "https://.*/skiptools/skip.git", from: ".*");.package(url: "https://skip.tools/skiptools/skip.git", from: "'${SEMVER_NEXT}'");g' "${SKIPHUBPKG}"
-#sed -I '' 's;.package(url: "https://.*/skip.git", from: ".*");.package(url: "https://source.skip.tools/skip.git", from: "'${SEMVER_NEXT}'");g' "README.md"
-#sed -I '' 's;.package(url: "https://.*/skip.git", from: ".*");.package(url: "https://source.skip.tools/skip.git", from: "'${SEMVER_NEXT}'");g' "${SKIPHUBPKG}"
+SKIP_ARTIFACT_ZIP="skip.zip"
+echo "Building ${SKIP_ARTIFACT_ZIP}"
+SKIPLOCAL=.. swift build --arch arm64 --arch x86_64 --configuration ${SKIPCONFIG} --product skip
+
+cd .build/apple/Products/${SKIPCONFIG}/
+# ensure we can run the skip command
+./skip version
+zip -9 ${RELSTAGING}/${SKIP_ARTIFACT_ZIP} skip
+SKIPCMD_CHECKSUM=$(shasum -a 256 ${RELSTAGING}/${SKIP_ARTIFACT_ZIP} | cut -f 1 -d ' ')
+cd -
+
+# package.targets += [.binaryTarget(name: "${PRODUCT}", url: "${ARTIFACT_URL}", checksum: "${PLUGIN_CHECKSUM}")]
+sed -I '' 's;package.targets += \[.binaryTarget.*;package.targets += [.binaryTarget(name: "'${ARTIFACT}'", url: "'${ARTIFACT_URL}'", checksum: "'${PLUGIN_CHECKSUM}'")];g' ${SKIPPKG}
+
+SKIPDRIVE_VERSION_PATH="Sources/SkipDrive/Version.swift"
+
+sed -I '' 's;public let skipVersion = .*;public let skipVersion = "'${SKIP_VERSION}'";g' "${SKIPDRIVE_VERSION_PATH}"
+
+git add Package.swift ${README_PATH} ${SKIPDRIVE_VERSION_PATH}
+git add .
+git commit -m "Release ${SKIP_VERSION}"
+git tag --sign "${SKIP_VERSION}" -m "Release ${SKIP_VERSION}"
+git push --follow-tags
+
+sed -I '' 's;.package(url: "https://.*/skiptools/skip.git", from: ".*");.package(url: "https://skip.tools/skiptools/skip.git", from: "'${SKIP_VERSION}'");g' "README.md"
+sed -I '' 's;.package(url: "https://.*/skiptools/skip.git", from: ".*");.package(url: "https://skip.tools/skiptools/skip.git", from: "'${SKIP_VERSION}'");g' "${SKIPHUBPKG}"
 
 # also grab the latest skiphub version and update it in the README
 SKIPHUB_VERSION=`git ls-remote --tags https://github.com/skiptools/skiphub | awk -F/ '$NF ~ /^v?[0-9]+\.[0-9]+\.[0-9]+$/ {print $NF}' | sort -V | tail -n1`
 
-#sed -I '' 's;.package(url: "https://github.com/skiptools/skiphub.git", from: ".*");.package(url: "https://github.com/skiptools/skiphub.git", from: "'${SKIPHUB_VERSION}'");g' "README.md"
+cp -a ${SKIPSTONEDIR}/${DIR}/${PLUGIN_ZIP} ${RELSTAGING}
 
-git add Package.swift ${README_PATH}
-git add .
-git commit -m "Release ${SEMVER_NEXT}"
-git tag --sign "${SEMVER_NEXT}" -m "Release ${SEMVER_NEXT}"
-git push --follow-tags
+RELNOTES="${RELSTAGING}/releasenotes.md"
+#echo "Updating relnotes: ${RELNOTES}"
+cat > ${RELNOTES} << EOF
+Skip release ${SKIP_VERSION}.
+EOF
 
+cd ${RELSTAGING}
+
+echo '```' >> ${RELNOTES}
+shasum -a 256 *.zip >> ${RELNOTES}
+echo '```' >> ${RELNOTES}
+
+cat ${RELNOTES}
+
+gh release -R github.com/skiptools/skip create -F releasenotes.md "${SKIP_VERSION}" *.zip
 cd '-'
-# back in SKIPPKGDIR
-
-SKIPDRIVE_VERSION_PATH="Sources/SkipDrive/Version.swift"
-
-sed -I '' 's;public let skipVersion = .*;public let skipVersion = "'${SEMVER_NEXT}'";g' "${SKIPDRIVE_VERSION_PATH}"
-
-swift build --arch arm64 --arch x86_64 --configuration ${SKIPCONFIG} --product skip
-
-cd .build/apple/Products/${SKIPCONFIG}/
-# ensure we can run it
-./skip version
-zip ../../../../skip.zip skip
-cd -
-
-git add "${SKIPDRIVE_VERSION_PATH}"
-git commit -m "Release ${SEMVER_NEXT}"
-git tag --sign "${SEMVER_NEXT}" -m "Release ${SEMVER_NEXT}"
-git push --follow-tags
-
-# Now when we upload, it will be to the tag that corresponds to this download
-#echo "Creating release artifact: ${ARTIFACT_URL}"
-gh release -R github.com/skiptools/skip create --notes "" "${SEMVER_NEXT}" ${DIR}/${PLUGIN_BASENAME}.zip skip.zip
 
 echo "Waiting to download to become available…"
 sleep 15
 
 # sometimes need to wait briefly for the artifact to become available
-curl --location --fail --retry 5 --retry-all-errors --retry-max-time 120 -o /dev/null "${ARTIFACT_URL}"
+curl --location --fail --retry 10 --retry-all-errors --retry-max-time 120 -o /dev/null "${ARTIFACT_URL}"
 
 # now jump *back* to the package and make sure we can run the command
-cd '-'
 if [ "${SKIPPKG}" != "/dev/null" ]; then        
     swift package --disable-sandbox --allow-writing-to-package-directory skip info
 fi
 
-# finally, jump back and make a corresponding release in the private SwiftSource
-cd '-'
+# jump back and make a corresponding release in skipstone
+cd ${SKIPSTONEDIR}
 
-echo "Hit return to tag ${VERSION_PATH}"
-
-git add "${VERSION_PATH}"
-git commit -m "Release ${SEMVER_NEXT}" "${VERSION_PATH}"
-git tag --sign "${SEMVER_NEXT}" -m "Release ${SEMVER_NEXT}"
+git add "${SKIPSTONE_VERSION_PATH}"
+git commit -m "Release ${SKIP_VERSION}"
+git tag --sign "${SKIP_VERSION}" -m "Release ${SKIP_VERSION}"
 git push --follow-tags
 
-# get the new ref after we have pushed to skiptool
-GITREF="$(git rev-parse HEAD)"
+# update the homebrew cask with the updated skip command
+cd ${SKIPBREWDIR}
 
+sed -I '' "s;version \".*\";version \"${SKIP_VERSION}\";g" Casks/skip.rb
+sed -I '' "s;sha256 \".*\";sha256 \"${SKIPCMD_CHECKSUM}\";g" Casks/skip.rb
+git add Casks/skip.rb
+git commit -m "Release ${SKIP_VERSION}" 
+git tag --sign "${SKIP_VERSION}" -m "Release ${SKIP_VERSION}"
+git push --follow-tags
 
-RELNOTES=`mktemp`
-#echo "Updating relnotes: ${RELNOTES}"
-cat > ${RELNOTES} << EOF
-head: ${GITREF}
-checksum: ${CHECKSUM}
-EOF
-
-#echo "updating relnotes: ${RELNOTES}"
-gh release -R github.com/skiptools/skip edit "${SEMVER_NEXT}" -F ${RELNOTES} 
-#echo "done releasing ${SEMVER_NEXT}"
+brew update skiptools/skip/skip
+skip version
 
