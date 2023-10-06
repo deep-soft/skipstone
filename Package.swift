d25482876d5cb649e43518fbe928c93438189ec8
marcprux <mwp1@cornell.edu>
2023-10-06 15:11:03 -0400

    Update swift-tools-support-core version

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.3"),
-        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.5.2"),
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.6.1"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.6.0"),
         .package(url: "https://github.com/marcprux/universal.git", from: "5.2.0"),
     ],
