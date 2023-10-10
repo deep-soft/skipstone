1f1a5bfab2420bfaf5aeaafa3d7cb33957860a28
marcprux <mwp1@cornell.edu>
2023-10-09 23:52:21 -0400

    Release 0.6.119

--- Sources/SkipSyntax/Version.swift
+++ Sources/SkipSyntax/Version.swift
@@ -2,4 +2,4 @@
 // NOTE: this variable is updated by the `scripts/release_skip.sh` script when the version is bumped.
 
 /// The current version of Skip.
-public let skipVersion = "0.6.118"
+public let skipVersion = "0.6.119"
