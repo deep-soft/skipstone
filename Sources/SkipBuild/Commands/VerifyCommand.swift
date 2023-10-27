e7afdd5e9fcfd46f99f502dec6568079bcc7308f
marcprux <mwp1@cornell.edu>
2023-10-27 16:47:45 -0400

    Fix skip verify

--- Sources/SkipBuild/Commands/VerifyCommand.swift
+++ Sources/SkipBuild/Commands/VerifyCommand.swift
@@ -54,8 +54,9 @@ struct VerifyCommand: SkipCommand, StreamingCommand, ToolOptionsCommand {
 
         //await checkVersion(title: "ECHO2 VERSION", cmd: ["sh", "-c", "echo ONE ; sleep 1; echo TWO ; sleep 1; echo THREE ; sleep 1; echo 3.2.1"], min: Version("1.2.3"), pattern: "([0-9.]+)", watch: true)
 
-        //await checkVersion(title: "Skip version", cmd: [toolOp, "version"], min: Version("0.6.4"), pattern: "Skip version ([0-9.]+)")
-        await checkVersion(title: "macOS version", cmd: ["XXXX", "--productVersion"], min: Version("13.5.0"), pattern: "([0-9.]+)")
+        await checkVersion(title: "Skip version", cmd: ["skip", "version"], min: Version(skipVersion), pattern: "Skip version ([0-9.]+)")
+
+        //await checkVersion(title: "macOS version", cmd: ["XXXX", "--productVersion"], min: Version("13.5.0"), pattern: "([0-9.]+)")
 
 
         let messages = await out.elements
