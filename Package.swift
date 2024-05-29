447cdf73d8bee27d2b22e643954c6f154b70bcc5
marcprux <mwp1@cornell.edu>
2024-05-29 09:23:56 -0400

    Bump swift-syntax to 510.0.2 and swift-argument-parser to 1.4.0

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ let package = Package(
         .executable(name: "SkipKey", targets: ["SkipKey"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.2"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
         .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.6.1"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.2.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.4.0"),
         .package(url: "https://github.com/marcprux/universal.git", from: "5.2.2"),
     ],
     targets: [
