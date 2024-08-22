95d78f3c85de407368501b8041edf5a50a68871e
Abe White <abewhite@mac.com>
2024-08-22 14:51:02 -0500

    Release 1.0.5

--- Sources/SkipSyntax/Version.swift
+++ Sources/SkipSyntax/Version.swift
@@ -2,4 +2,4 @@
 // NOTE: this variable is updated by the `scripts/release_skip.sh` script when the version is bumped.
 
 /// The current version of Skip.
-public let skipVersion = "1.0.4"
+public let skipVersion = "1.0.5"
