5845e567b3592d327769401fd47901e82a061735
Marc Prud'hommeaux <marc@skip.tools>
2025-03-01 16:46:52 -0500

    Update generated AppDelegate to accomodate AppKit as well as UIKit applications

--- Sources/SkipBuild/SkipProject.swift
+++ Sources/SkipBuild/SkipProject.swift
@@ -1745,9 +1745,7 @@ New features and better performance.
 
         /// The entry point to the app simply loads the App implementation from SPM module.
         @main struct AppMain: App {
-            #if canImport(UIKit)
-            @UIApplicationDelegateAdaptor(AppMainDelete.self) var appDelegate
-            #endif
+            @AppDelegateAdaptor(AppMainDelegate.self) var appDelegate
             @Environment(\\.scenePhase) private var scenePhase
 
             var body: some Scene {
@@ -1757,11 +1755,11 @@ New features and better performance.
                 .onChange(of: scenePhase) { oldPhase, newPhase in
                     switch newPhase {
                     case .active:
-                        \(primaryModuleName)AppDelegate.shared.onResume(appDelegate.application!)
+                        AppDelegate.shared.onResume(appDelegate.application)
                     case .inactive:
-                        \(primaryModuleName)AppDelegate.shared.onPause(appDelegate.application!)
+                        AppDelegate.shared.onPause(appDelegate.application)
                     case .background:
-                        \(primaryModuleName)AppDelegate.shared.onStop(appDelegate.application!)
+                        AppDelegate.shared.onStop(appDelegate.application)
                     @unknown default:
                         print("unknown app phase: \\(newPhase)")
                     }
@@ -1769,25 +1767,44 @@ New features and better performance.
             }
         }
 
+        typealias AppDelegate = \(primaryModuleName)AppDelegate
         #if canImport(UIKit)
-        class AppMainDelete: UIResponder, UIApplicationDelegate {
-            unowned var application: UIApplication? = nil
+        typealias AppDelegateAdaptor = UIApplicationDelegateAdaptor
+        typealias AppMainDelegateBase = UIApplicationDelegate
+        typealias AppType = UIApplication
+        #elseif canImport(AppKit)
+        typealias AppDelegateAdaptor = NSApplicationDelegateAdaptor
+        typealias AppMainDelegateBase = NSApplicationDelegate
+        typealias AppType = NSApplication
+        #endif
 
-            func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
-                self.application = application
-                \(primaryModuleName)AppDelegate.shared.onStart(application)
+        class AppMainDelegate: NSObject, AppMainDelegateBase {
+            let application = AppType.shared
+
+            #if canImport(UIKit)
+            func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+                AppDelegate.shared.onStart(application)
                 return true
             }
 
             func applicationWillTerminate(_ application: UIApplication) {
-                \(primaryModuleName)AppDelegate.shared.onDestroy(application)
+                AppDelegate.shared.onDestroy(application)
             }
 
             func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
-                \(primaryModuleName)AppDelegate.shared.onLowMemory(application)
+                AppDelegate.shared.onLowMemory(application)
             }
+            #elseif canImport(AppKit)
+            func applicationWillFinishLaunching(_ notification: Notification) {
+                AppDelegate.shared.onStart(application)
+            }
+
+            func applicationWillTerminate(_ application: Notification) {
+                AppDelegate.shared.onDestroy(application)
+            }
+            #endif
+
         }
-        #endif
 
         """
         try appMainContents.write(to: primaryModuleAppMainURL.createParentDirectory(), atomically: false, encoding: .utf8)
