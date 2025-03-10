fee40852bc8273d3056a0b21e1bdf015bad82ad6
Marc Prud'hommeaux <marc@skip.tools>
2025-03-10 10:04:12 -0400

    Add tools.skip.** to keep list for generated proguard-rules.pro file

--- Sources/SkipBuild/SkipProject.swift
+++ Sources/SkipBuild/SkipProject.swift
@@ -3022,6 +3022,7 @@ extension FrameworkProjectLayout {
         """
         -keeppackagenames **
         -keep class skip.** { *; }
+        -keep class tools.skip.** { *; }
         -keep class kotlin.jvm.functions.** {*;}
         -keep class com.sun.jna.** { *; }
         -keep class * implements com.sun.jna.** { *; }
